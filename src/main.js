27c87bb06f1a3af71d6e8a9bf39eaa52ff8008db
Reese Lee <reese.lee.cy@gmail.com>
2019-05-31 15:01:39 -0700

    add basic front end, but commented out

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import { Galactic } from './galactic.js';
-import $ from 'jquery';
+// import { Galactic } from './galactic.js';
+// import $ from 'jquery';
 
-$(document).ready(function() {
-  $("form#dayDate").submit(function(event) {
-    event.preventDefault();
-    let birthday = $("form#dayDate").val().toString();
-    let dateSolver = new Galactic(birthday);
-    let results = dateSolver.dayFinder(birthday);
-    $("#result").text(results).show();
-  });
-});
+// $(document).ready(function() {
+//   $("form#dayDate").submit(function(event) {
+//     event.preventDefault();
+//     let birthday = $("form#dayDate").val().toString();
+//     let dateSolver = new Galactic(birthday);
+//     $("#result").text(dateSolver).show();
+//   });
+// });
