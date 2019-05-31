0c75d77b559fb9cb453f71b8a026c435aa2ee383
Reese Lee <reese.lee.cy@gmail.com>
2019-05-31 12:44:09 -0700

    move backend logic from main.js to its own class file

---
+++ src/galactic.js
@@ -0,0 +1,19 @@
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/scss/bootstrap.scss';
+// import './sass/styles.scss';
+//
+export class Galactic {
+  constructor(birthday) {
+    this.birthday = new Date(birthday);
+  }
+
+  getAge() {
+    let currentDate = new Galactic();
+    let currentYear = currentDate.getFullYear();
+    let age = currentYear - (this.birthday.GetFullYear());
+    return age;
+  }
+
+  
+}
