c810128fe480fd9d3c6b4d36b32ef355c3687a0d
Reese Lee <reese.lee.cy@gmail.com>
2019-05-31 10:26:47 -0700

    add constructor to main.js to pass first test

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/scss/bootstrap.scss';
+// import './sass/styles.scss';
+//
+export class Galactic {
+  constructor(birthday) {
+    this.birthday = new Date(birthday);
+  }
+
+  
+}
